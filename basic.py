1932c35bb6afa3758929a1bb24f2ee78284376d8
wellecks <cis455@vm.(none)>
2014-03-31 23:42:51 -0400

    First commit.

---
+++ basic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import optparse
+import codecs
+import sys
+
+optparser = optparse.OptionParser()
+optparser.add_option("-f", "--unsegmented-input-file", dest="f", default="data/training_unseg.utf8", help="N-best lists")
+optparser.add_option("-n", "--num-training-sentences", dest="n", default=sys.maxint, type="int", help="Number of training sentences (default=all)")
+optparser.add_option("-o", "--output-file", dest="o", default="output/basic_output.utf8", help="Number of training sentences (default=all)")
+(opts,_) = optparser.parse_args()
+
+# read in n lines of the given utf8 file
+space = '  '
+train = [line for line in codecs.open(opts.f, 'r', encoding='utf-8')][:opts.n]
+seg_train = [space.join(line) for line in train]
+punctuation = [u'\uff0c', u'\uff0e']
+with open(opts.o, 'wb') as f:
+	for line in seg_train: 
+		f.write(line.encode('utf-8').replace('\r', ''))
