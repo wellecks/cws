442c6b66e02e02e2241130dce14ce0e114e8e697
wellecks <wellecks@gmail.com>
2014-04-06 21:38:17 -0400

    Created additional feature functions.

--- basic.py
+++ basic.py
@@ -3,7 +3,7 @@ import optparse
 import codecs
 import sys
 import unicodedata
-
+import random
 optparser = optparse.OptionParser()
 optparser.add_option("-f", "--unsegmented-input-file", dest="f", default="data/training_unseg.utf8", help="N-best lists")
 optparser.add_option("-n", "--num-training-sentences", dest="n", default=sys.maxint, type="int", help="Number of training sentences (default=all)")
@@ -13,7 +13,6 @@ optparser.add_option("-o", "--output-file", dest="o", default="output/basic_outp
 # read in n lines of the given utf8 file
 space = '  '
 train = [line for line in codecs.open(opts.f, 'r', encoding='utf-8')][:opts.n]
-		
 seg_train = [space.join(line) for line in train]
 with open(opts.o, 'wb') as f:
 	for line in seg_train: 
---
+++ features.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+A feature extractor for chunking.
+Copyright 2010,2011 Naoaki Okazaki.
+
+Modified by Sean Welleck for use in Chinese Word Segmentation application.
+"""
+
+# Separator of field values.
+separator = ' '
+
+# Field names of the input data.
+fields = 'c y'
+
+# Attribute templates.
+templates = (
+    (('c', -2), ),
+    (('c', -1), ),
+    (('c',  0), ),
+    (('c',  1), ),
+    (('c',  2), ),
+    (('c', -2), ('c', -1)),
+    (('c', -2), ('c',  0)),
+    (('c', -1), ('c',  0)),
+    (('c',  0), ('c',  1)),
+    (('c', -1), ('c',  1)),
+    (('c', -1), ('c',  0), ('c',  1)),
+    )
+
+import crfutils
+import unicodedata
+
+# Return whether the given character is punctuation.
+# Specifically, whether it is a member of a Unicode 
+# punctuation character class.
+def is_punct(c):
+	punct_categories = ['Pc', 'Pd', 'Pe', 'Pf', 'Pi', 'Po', 'Ps']
+    c = c.decode('utf-8')
+    if unicodedata.category(c) in punct_categories:
+        return "1"
+    else:
+        return "0"
+
+# Return whether the given character's "class", where
+# class = 1 if numeric, 2 if english letter, 3 otherwise.
+def char_class(c):
+	num_categories = ['Nl', 'No', 'Nd']
+	letter_categories = ['LC', 'Ll', 'Lm', 'Lm', 'Lo', 'Lt', 'Lu' ]
+	cat = unicodedata.category(c.decode('utf-8'))
+	if cat in num_categories:
+		return "1"
+	elif cat in letter_categories:
+		return "2"
+	else:
+		return "3"
+
+def feature_extractor(X):
+    # Apply attribute templates to obtain features.
+    crfutils.apply_templates(X, templates)
+    # Add the is_punct() feature.
+    map(lambda x: x['F'].append("punct=" + is_punct(x['c'])), X)
+    # Add the char_class() feature.
+    map(lambda x: x['F'].append("class=" + char_class(x['c'])), X)
+
+if __name__ == '__main__':
+    crfutils.main(feature_extractor, fields=fields, sep=separator)
